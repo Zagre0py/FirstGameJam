d6d007c2cfe9f7c48c413cd5856ce5fd509658a5
Zagre0py <david.rodriguez.daza21@gmail.com>
2025-03-03 23:43:27 -0500

    VidaMosca/AniamcionBarra
    Se agrego vida a la mosca y animacion a la vida para que se reduzca al detectar un collider

--- Assets/AssetsSantiago/Scripts/NavMeshController.cs
+++ Assets/AssetsSantiago/Scripts/NavMeshController.cs
@@ -8,12 +8,14 @@ public class NavMeshController : MonoBehaviour
     public Transform objetivo;
     private Animator animator;
     private NavMeshAgent agente;
-    public float attackDistance = 5f;
+    private bool estaAtacando = false;
+    public float attackDistance;
 
 
     void Start()
     {
         agente = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -26,7 +28,13 @@ public class NavMeshController : MonoBehaviour
 
                 agente.isStopped = true;
             }
-            animator.SetTrigger("Attack");
+            estaAtacando = true;
+            animator.SetBool("Ataque", estaAtacando);
+        }
+        else{
+            agente.isStopped = false;
+            estaAtacando = false;
+            animator.SetBool("Ataque", estaAtacando);
         }
         agente.destination = objetivo.position;
     }
