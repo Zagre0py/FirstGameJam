e8fddfe5bbae040fc5df37fd000042bbab232922
FebaDev <felipebposada@gmail.com>
2025-03-04 10:44:11 -0500

    UI ScreenGame
    Add Health Bar

--- Assets/AssetsFelipe/ScriptsFelipe/LifePlayer.cs
+++ Assets/AssetsFelipe/ScriptsFelipe/LifePlayer.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class LifePlayer : MonoBehaviour
 {
    public float lifeMax;
    public float lifeCurrent;
+    public Image barLife;
 
     private void Start()
     {
         lifeMax = lifeCurrent;
+        barLife.fillAmount = lifeCurrent / lifeMax;
         lifeMax = 10;
+
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -19,6 +22,7 @@ public class LifePlayer : MonoBehaviour
             TakeDamage();
             Debug.Log("El player  esta recibiendo daño" + lifeCurrent);
         }
+        barLife.fillAmount = lifeCurrent / lifeMax;
     }
     public void TakeDamage()
     {
